4028cbeff5883e766f5fcc44227aa621341ed97a
CROUDZ <giov.di.via@gmail.com>
2025-08-14 21:07:09 +0200

    fix image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	// Enable static export and write an export manifest after build
+	output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    optimizeCss: true,
+  },
 };
 
 export default nextConfig;
